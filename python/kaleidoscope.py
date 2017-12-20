af36e4c57c885f97cf3a24942a035865d67ee89c
noseglasses <shinynoseglasses@gmail.com>
2017-12-20 17:44:07 +0100

    Some work on the python interface

--- python/kaleidoscope.py
+++ python/kaleidoscope.py
@@ -1023,3 +1023,61 @@ class Test(object):
       self.out.write("########################################################\n")
       self.out.write("~ %s ~\n" % msg)
       self.out.write("########################################################\n")
+      
+   def graphical_map(self):
+      
+      #left_upper = '\x250F'.decode("latin-1")   # ┏
+      #upper = '\x2501'.decode("latin-1")        # ━
+      #right_upper = '\x2513'.decode("latin-1")  # ┓
+      #right = '\x2503'.decode("latin-1")        # ┃
+      #right_bottom = '\x251b'.decode("latin-1") # ┛
+      #bottom = '\x2501'.decode("latin-1")       # ━
+      #left_bottom = '\x2517'.decode("latin-1")  # ┗
+      #left = '\x2503'.decode("latin-1")         # ┃
+      
+      #left_gate = '\x2523'.decode("latin-1")    # ┣
+      #top_gate = '\x2533'.decode("latin-1")     # ┳
+      #right_gate = '\x252b'.decode("latin-1")   # ┫
+      #bottom_gate = '\x253b'.decode("latin-1")  # ┻
+      
+      #cross = '\x254b'.decode("latin-1")        # ╋
+      
+      #keylist = ...
+      (r0c0, r0c1, r0c2, r0c3, r0c4, r0c5, r0c6, r0c7, r0c8, r0c9, r0c10, r0c11, r0c12, r0c13, r0c14, r0c15, \
+       r1c0, r1c1, r1c2, r1c3, r1c4, r1c5, r1c6, r1c7, r1c8, r1c9, r1c10, r1c11, r1c12, r1c13, r1c14, r1c15, \
+       r2c0, r2c1, r2c2, r2c3, r2c4, r2c5, r2c6, r2c7, r2c8, r2c9, r2c10, r2c11, r2c12, r2c13, r2c14, r2c15, \
+       r3c0, r3c1, r3c2, r3c3, r3c4, r3c5, r3c6, r3c7, r3c8, r3c9, r3c10, r3c11, r3c12, r3c13, r3c14, r3c15) =
+      tuple(keylist)
+      
+self.out.write(u"┏━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┓        ┏━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┳━━━━┓\n".format(r0c0, r0c1, r0c2, r0c3, r0c4, r0c5, r0c6,        r0c9,  r0c10, r0c11, r0c12, r0c13, r0c14, r0c15))
+self.out.write(u"┃{ 0}┃{ 1}┃{ 2}┃{ 3}┃{ 4}┃{ 5}┃{ 6}┃        ┃{ 7}┃{ 8}┃{ 9}┃{10}┃{11}┃{12}┃{13}┃\n")
+self.out.write(u"┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫    ┃        ┃    ┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫\n")
+self.out.write(u"┃{ 0}┃{ 1}┃{ 2}┃{ 3}┃{ 4}┃{ 5}┣━━━━┫        ┣━━━━┫{ 6}┃{ 7}┃{ 8}┃{ 9}┃{10}┃{11}┃\n".format(r1c0, r1c1, r1c2, r1c3, r1c4, r1c5, r1c10, r1c11, r1c12, r1c13, r1c14, r1c15))
+self.out.write(u"┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫    ┃        ┃    ┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫\n")
+self.out.write(u"┃{ 0}┃{ 1}┃{ 2}┃{ 3}┃{ 4}┃{ 5}┃{ 6}┃        ┃{ 7}┃{ 8}┃{ 9}┃{10}┃{11}┃{12}┃{13}┃\n".format(r2c0, r2c1, r2c2, r2c3, r2c4, r2c5, r1c6, r1c9, r2c10, r2c11, r2c12, r2c13, r2c14, r2c15))
+self.out.write(u"┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫        ┣━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━╋━━━━┫\n")
+self.out.write(u"┃{ 0}┃{ 1}┃{ 2}┃{ 3}┃{ 4}┃{ 5}┃{ 6}┃        ┃{ 7}┃{ 8}┃{ 9}┃{10}┃{11}┃{12}┃{13}┃\n".format(r3c0, r3c1, r3c2, r3c3, r3c4, r3c5, r2c6,        r2c9,  r3c10, r3c11, r3c12, r3c13, r3c14, r3c15))
+self.out.write(u"┗━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┛        ┗━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┻━━━━┛\n")
+self.out.write(u"                ┏━━━━┓                                    ┏━━━━┓                \n")
+self.out.write(u"                ┃{ 0}┣━━━━┓                          ┏━━━━┫{ 1}┃                \n".format(r0c7, r0c8))
+self.out.write(u"                ┗━━━━┫{ 0}┣━━━━┓                ┏━━━━┫{ 1}┣━━━━┛                \n".format(r1c7, r1c8))
+self.out.write(u"                     ┗━━━━┫{ 0}┣━━━━┓      ┏━━━━┫{ 1}┣━━━━┛                     \n".format(r2c7, r2c8))
+self.out.write(u"                          ┗━━━━┫{ 0}┃      ┃{ 1}┣━━━━┛                          \n".format(r3c7, r3c8))
+self.out.write(u"                               ┗━━━━┛      ┗━━━━┛                               \n")
+self.out.write(u"                    ┏━━━━━━┓                        ┏━━━━━━┓                    \n")
+self.out.write(u"                    ┃ { 0} ┃                        ┃ { 1} ┃                    \n".format(r3c6, r3c9))
+self.out.write(u"                    ┗━━━━━━┛                        ┗━━━━━━┛                    \n")
+
+##define KEYMAP(                                                                                     \
+  #r0c0, r0c1, r0c2, r0c3, r0c4, r0c5, r0c6,        r0c9,  r0c10, r0c11, r0c12, r0c13, r0c14, r0c15, \
+  #r1c0, r1c1, r1c2, r1c3, r1c4, r1c5, r1c6,        r1c9,  r1c10, r1c11, r1c12, r1c13, r1c14, r1c15, \
+  #r2c0, r2c1, r2c2, r2c3, r2c4, r2c5,                     r2c10, r2c11, r2c12, r2c13, r2c14, r2c15, \
+  #r3c0, r3c1, r3c2, r3c3, r3c4, r3c5, r2c6,        r2c9,  r3c10, r3c11, r3c12, r3c13, r3c14, r3c15, \
+              #r0c7, r1c7, r2c7, r3c7,                             r3c8,  r2c8,  r1c8, r0c8,         \
+                          #r3c6,                                          r3c9)                      \
+  #{                                                                                                 \
+    #{r0c0, r0c1, r0c2, r0c3, r0c4, r0c5, r0c6, r0c7, r0c8, r0c9, r0c10, r0c11, r0c12, r0c13, r0c14, r0c15}, \
+    #{r1c0, r1c1, r1c2, r1c3, r1c4, r1c5, r1c6, r1c7, r1c8, r1c9, r1c10, r1c11, r1c12, r1c13, r1c14, r1c15}, \
+    #{r2c0, r2c1, r2c2, r2c3, r2c4, r2c5, r2c6, r2c7, r2c8, r2c9, r2c10, r2c11, r2c12, r2c13, r2c14, r2c15}, \
+    #{r3c0, r3c1, r3c2, r3c3, r3c4, r3c5, r3c6, r3c7, r3c8, r3c9, r3c10, r3c11, r3c12, r3c13, r3c14, r3c15}, \
+  #}
