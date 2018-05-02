bede6ae52781f822fcc458fafc45ccc8d2f6f914
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-02 12:05:28 +0000

    Deploy CapeLeidokos/Leidokos-Python to github.com/CapeLeidokos/Leidokos-Python.git:gh-pages

---
+++ API/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '52ed6ca1149cacc8cfae39c766d7dd24e15afaff',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
